84860331489a72d41b17c724bdca6992b93ab3bc
Charro Gruver <null>
2024-01-14 22:34:08 +0000

    add stargate components

---
+++ init.sh
@@ -0,0 +1,2 @@
+oc import-image stargate-docsapi:v2 --from=docker.io/stargateio/docsapi:v2 --confirm -n dev-tools
+oc import-image stargate-coordinator-4_0:v2 --from=docker.io/stargateio/coordinator-4_0:v2 --confirm -n dev-tools
